8f9d46a4781911497c0fadbc97e0c0013f891dc1
Jose Alvarado <jalvarado80@gmail.com>
2016-07-30 18:13:02 -0700

    added birthYear()

--- src/Person.js
+++ src/Person.js
@@ -1,10 +1,15 @@
 class Person {
-    constructor(name) {
+    constructor(name, age = 1900) {
         this.name = name || 'John';
+        this.age = age;
     }
 
     speak() {
         return `Hello, my name is ${this.name}!`;
+    }
+
+    birthYear() {
+        return (this.age !== 1900) ? new Date().getFullYear() - this.age : this.age;
     }
 }
 
--- test/PersonSpec.js
+++ test/PersonSpec.js
@@ -3,11 +3,24 @@ import Person from '../src/Person';
 chai.should();
 
 describe('Person', () => {
-    describe('person.speak()', () => {
-        it('should speak', () => {
+    describe('when a name is given', () => {
+        it('should speak given name', () => {
             const temp = new Person('Jose');
             const result = temp.speak();
             result.should.equal('Hello, my name is Jose!');
+        });
+        it('should speak given default name', () => {
+            const temp = new Person();
+            const result = temp.speak();
+            result.should.equal('Hello, my name is John!');
+        });
+    });
+
+    describe('when an age is given', () => {
+        it('should equal 1900', () => {
+            const temp = new Person('Jose');
+            const result = temp.birthYear();
+            result.should.equal(1900);
         });
     });
 });
