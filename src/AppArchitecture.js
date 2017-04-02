d2f37d5cd517fb3503399456812b8a63b51f5ea9
Jose Alvarado <jalvarado80@gmail.com>
2017-04-01 17:48:44 -0700

    working

--- src/AppArchitecture.js
+++ src/AppArchitecture.js
@@ -2,31 +2,38 @@ const registry = (() =>
 {
   const modules = new Map();
   return {
-    register: (moduleName, module) => {
-      console.log(`Registering module: ${moduleName}`);
-      modules.set(moduleName, module);
+    register: (pckg) => {
+      console.log(`Registering module: ${pckg.name}`);
+
+      pckg.module.notify = ((msg) => {
+        console.log(`${pckg.name}: ${msg}`);
+      });
+
+      modules.set(pckg.name, pckg.module);
     },
     require: (module) => modules.get(module)
   }
 })();
 
-const aModule = { name: "aModule", module: {}}
+const aModule = { name: "aModule", module: { }}
 const logger = { name: "logger", module: { log: (msg) => console.log(msg) } }
 const request = { name: "request", module: { get: (msg) => console.log(msg) } }
 const modules = [aModule, logger, request];
 
-let app = (registry, modules) =>
+let appCore = ((registry, modules) =>
 {
-  modules.forEach((item) => registry.register(item.name, item.module));
+  modules.forEach((item) => registry.register(item));
   return {
-    register: (name, module) => registry.register(name, module)
+    register: (item) => registry.register(item),
+
+    notifyAll: (msg) => modules.forEach((pckg) => {
+      pckg.module.notify(msg)
+    })
   }
-};
-
-app = app(registry, modules);
+})(registry, modules);
 
 
-const sandbox = (registry) =>
+const sandbox = ((registry) =>
 {
   const request = registry.require("request");
   const logger = registry.require("logger");
@@ -35,9 +42,7 @@ const sandbox = (registry) =>
     log: (msg) => logger.log(msg),
     get: (url) => request.get(url)
   }
-}
+})(registry);
 
-const sbx = sandbox(registry);
-app.register("sandbox", sbx);
-
-sbx.log("blah")
+appCore.register({name: "sandbox", module: sandbox});
+appCore.notifyAll("OMG This Works!");
