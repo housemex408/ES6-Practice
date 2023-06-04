f2d3a9a0219165fec7ad73f9c757e2b604c82880
Jose Alvarado <jalvarado80@gmail.com>
2023-06-03 18:25:42 -0700

    fixed use of this

--- src/Ratios/Ratios.js
+++ src/Ratios/Ratios.js
@@ -1,58 +1,74 @@
 
 let arr = [1, 1, 0, -1, -1];
 
-// const ratios = {
-//     positive: 0,
-//     negative: 0,
-//     zero:0,
-//     print: () => {
-//         console.log(`${ratios.positive.toFixed(6)}`);
-//         console.log(`${ratios.negative.toFixed(6)}`);
-//         console.log(`${ratios.zero.toFixed(6)}`);
-//     }
-// }
+const ratios = {
+    positive: 0,
+    negative: 0,
+    zero: 0,
+    calculateRatios: function (arr) {
+        let posC = 0;
+        let negC = 0
+        let zeroC = 0;
+       
+        arr.forEach(item => {
+            if (item > 0)
+                ++posC;
+            else if (item < 0)
+                ++negC;
+            else if (item == 0)
+                ++zeroC;
+        });
 
-// ratios.positive = arr.filter(p => p > 0).length / arr.length;
-// ratios.negative = arr.filter(p => p < 0).length / arr.length;
-// ratios.zero = arr.filter(p => p == 0).length / arr.length;
-
-// ratios.print();
-
-class Ratios {
-    #positive;
-    #negative;
-    #zero;
-    #arr;
-
-    constructor(arr) {
-        this.#positive = 0;
-        this.#negative = 0;
-        this.#zero = 0;
-        this.#arr = arr;
-    }
-
-    #getPositive () {
-        this.#positive = this.#arr.filter(p => p > 0).length / this.#arr.length;
-        return this.#positive.toFixed(6);
-    }
-
-    #getNegative () {
-        this.#negative = this.#arr.filter(p => p < 0).length / this.#arr.length;
-        return this.#negative.toFixed(6);
-    }
-
-    #getZero () {
-        this.#zero = this.#arr.filter(p => p == 0).length / this.#arr.length;
-        return this.#zero.toFixed(6);
-    }
-
-    print() {
-        console.log(this.#getPositive());
-        console.log(this.#getNegative());
-        console.log(this.#getZero());
+        this.positive = posC / arr.length;
+        this.negative = negC / arr.length;
+        this.zero = zeroC / arr.length;
+    },
+    print: function () {
+        console.log(`${this.positive.toFixed(6)}`);
+        console.log(`${this.negative.toFixed(6)}`);
+        console.log(`${this.zero.toFixed(6)}`);
     }
 }
 
-const ratios = new Ratios(arr);
+ratios.calculateRatios(arr);
 
 ratios.print();
+
+// class Ratios {
+//     #positive;
+//     #negative;
+//     #zero;
+//     #arr;
+
+//     constructor(arr) {
+//         this.#positive = 0;
+//         this.#negative = 0;
+//         this.#zero = 0;
+//         this.#arr = arr;
+//     }
+
+//     #getPositive () {
+//         this.#positive = this.#arr.filter(p => p > 0).length / this.#arr.length;
+//         return this.#positive.toFixed(6);
+//     }
+
+//     #getNegative () {
+//         this.#negative = this.#arr.filter(p => p < 0).length / this.#arr.length;
+//         return this.#negative.toFixed(6);
+//     }
+
+//     #getZero () {
+//         this.#zero = this.#arr.filter(p => p == 0).length / this.#arr.length;
+//         return this.#zero.toFixed(6);
+//     }
+
+//     print() {
+//         console.log(this.#getPositive());
+//         console.log(this.#getNegative());
+//         console.log(this.#getZero());
+//     }
+// }
+
+// const ratios = new Ratios(arr);
+
+// ratios.print();
